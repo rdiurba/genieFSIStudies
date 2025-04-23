421b8d5f27568fe957c474bc6a2eaf56c65fcc97
Richard Diurba <rdiurba@dunegpvm12.fnal.gov>
2025-04-23 03:05:28 -0500

    Updated plotting wrappers

--- hadStudies/plotting/fitNuclMultKO.py
+++ hadStudies/plotting/fitNuclMultKO.py
@@ -1,26 +1,38 @@
 import os
-import numpy as np
+#import numpy as np
 #os.system("source ../setupNuSystematics.sh")
-tgt=[1000060120,1000260560,1000822080]
+tgt=[1000060120,1000260560,1000822080,1000180400]
 ke=[0.125,0.25,0.5]
 num=1000000
-probe=[2112,2212]
-seed=np.random.randint(0,10000)
+probe=[2212]
+#seed=np.random.randint(0,10000)
 seed=0
-os.system("rm nucleonKODifference.txt")
-os.system("rm nucleonKOSum.txt")
+os.system("rm nucleonKO*.txt")
 with open("nucleonKODifference.txt", "w") as f:
     f.write("probe,ke,target,meanhA,meanErrhA,stdhA,stdErrhA,meannhN,meanErrhN,stdhN,stdErrhN,meanINCL,meanErrINCL,stdINCL,stdErrINCL,meanGeant4,meanErrGeant4,stdGeant4,stdErrGeant4")
     f.write("\n")
 with open("nucleonKOSum.txt", "w") as f:
     f.write("probe,ke,target,consthA,constErrhA,slopehA,slopeErrhA,consthN,constErrhN,slopehN,slopeErrhN,constINCL,constErrINCL,slopeINCL,slopeErrINCL,constGeant4,constErrGeant4,slopeGeant4,slopeErrGeant4")
     f.write("\n")
+with open("nucleonKODifferenceThresh.txt", "w") as f:
+    f.write("probe,ke,target,meanhA,meanErrhA,stdhA,stdErrhA,meannhN,meanErrhN,stdhN,stdErrhN,meanINCL,meanErrINCL,stdINCL,stdErrINCL,meanGeant4,meanErrGeant4,stdGeant4,stdErrGeant4")
+    f.write("\n")
+with open("nucleonKOSumThresh.txt", "w") as f:
+    f.write("probe,ke,target,consthA,constErrhA,slopehA,slopeErrhA,consthN,constErrhN,slopehN,slopeErrhN,constINCL,constErrINCL,slopeINCL,slopeErrINCL,constGeant4,constErrGeant4,slopeGeant4,slopeErrGeant4")
+    f.write("\n")
+
+
+
+
+
 for particle in probe:
     for t in tgt:
         for k in ke:
             seed=seed+1
             os.system("root -l -b -q 'plotKODiff.C("+str(particle)+","+str(k)+","+str(t)+")'")
             os.system("root -l -b -q 'plotKOSum.C("+str(particle)+","+str(k)+","+str(t)+")'")
+            os.system("root -l -b -q 'plotKODiffThresh.C("+str(particle)+","+str(k)+","+str(t)+")'")
+            os.system("root -l -b -q 'plotKOSumThresh.C("+str(particle)+","+str(k)+","+str(t)+")'")
 
 
 
--- hadStudies/plotting/fitNuclMultPionAbs.py
+++ hadStudies/plotting/fitNuclMultPionAbs.py
@@ -1,18 +1,24 @@
 import os
-import numpy as np
+#import numpy as np
 #os.system("source ../setupNuSystematics.sh")
-tgt=[1000060120,1000260560,1000822080]
+tgt=[1000060120,1000260560,1000822080,1000180400]
 ke=[0.125,0.25,0.5]
 num=1000000
-probe=[111,211,-211]
-seed=np.random.randint(0,10000)
+probe=[211]
+#seed=np.random.randint(0,10000)
 seed=0
-os.system("rm pionAbsDifference.txt")
-os.system("rm pionAbsSum.txt")
+os.system("rm pionAbs*.txt")
 with open("pionAbsDifference.txt", "w") as f:
     f.write("probe,ke,target,meanhA,meanErrhA,stdhA,stdErrhA,meannhN,meanErrhN,stdhN,stdErrhN,meanINCL,meanErrINCL,stdINCL,stdErrINCL,meanGeant4,meanErrGeant4,stdGeant4,stdErrGeant4")
     f.write("\n")
 with open("pionAbsSum.txt", "w") as f:
+    f.write("probe,ke,target,meanhA,meanErrhA,stdhA,stdErrhA,meannhN,meanErrhN,stdhN,stdErrhN,meanINCL,meanErrINCL,stdINCL,stdErrINCL,meanGeant4,meanErrGeant4,stdGeant4,stdErrGeant4")
+    f.write("\n")
+
+with open("pionAbsDifferenceThresh.txt", "w") as f:
+    f.write("probe,ke,target,meanhA,meanErrhA,stdhA,stdErrhA,meannhN,meanErrhN,stdhN,stdErrhN,meanINCL,meanErrINCL,stdINCL,stdErrINCL,meanGeant4,meanErrGeant4,stdGeant4,stdErrGeant4")
+    f.write("\n")
+with open("pionAbsSumThresh.txt", "w") as f:
     f.write("probe,ke,target,meanhA,meanErrhA,stdhA,stdErrhA,meannhN,meanErrhN,stdhN,stdErrhN,meanINCL,meanErrINCL,stdINCL,stdErrINCL,meanGeant4,meanErrGeant4,stdGeant4,stdErrGeant4")
     f.write("\n")
 for particle in probe:
@@ -21,6 +27,9 @@ for particle in probe:
             seed=seed+1
             os.system("root -l -b -q 'plotAbsDiff.C("+str(particle)+","+str(k)+","+str(t)+")'")
             os.system("root -l -b -q 'plotAbsSum.C("+str(particle)+","+str(k)+","+str(t)+")'")
+            os.system("root -l -b -q 'plotAbsDiffThresh.C("+str(particle)+","+str(k)+","+str(t)+")'")
+            os.system("root -l -b -q 'plotAbsSumThresh.C("+str(particle)+","+str(k)+","+str(t)+")'")
+
 
 
 
